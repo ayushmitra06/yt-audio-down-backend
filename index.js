70bc64e2d13e90d1e5490b333f4c86e71d98f152
Ayush <ayushmitra06@gmail.com>
2025-04-10 22:02:00 +0530

    added ffmpeg test route

--- index.js
+++ index.js
@@ -32,7 +32,6 @@ app.get('/test-ffmpeg', (req, res) => {
   res.send(`FFmpeg Path: ${ffmpegPath} | Exists: ${fs.existsSync(ffmpegPath)}`);
 });
 
-
 // Endpoint: Fetch Playlist Info
 app.post('/get-playlist-info', async (req, res) => {
   const { playlistUrl } = req.body;
